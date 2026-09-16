cbcd5c0d691891b676736a6ff07336ab14e1a035
ch33efr1es <Marc.Floresmendoza@CIX.CSI.CUNY.EDU>
2026-09-15 23:06:03 -0400

    styling draft 2

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
 console.log("Static Foundations loaded");
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("[data-carousel]").forEach(function (carousel) {
+    var track = carousel.querySelector(".carousel-track");
+    var slides = Array.from(track.children);
+    var prevBtn = carousel.querySelector(".carousel-prev");
+    var nextBtn = carousel.querySelector(".carousel-next");
+    var dotsContainer = carousel.querySelector(".carousel-dots");
+    var index = 0;
+
+    if (slides.length <= 1) {
+      prevBtn.hidden = true;
+      nextBtn.hidden = true;
+      return;
+    }
+
+    var dots = slides.map(function (_, i) {
+      var dot = document.createElement("button");
+      dot.type = "button";
+      dot.className = "carousel-dot";
+      dot.setAttribute("aria-label", "Go to image " + (i + 1));
+      dot.addEventListener("click", function () {
+        goTo(i);
+      });
+      dotsContainer.appendChild(dot);
+      return dot;
+    });
+
+    function update() {
+      track.style.transform = "translateX(" + index * -100 + "%)";
+      dots.forEach(function (dot, i) {
+        dot.classList.toggle("active", i === index);
+      });
+    }
+
+    function goTo(i) {
+      index = (i + slides.length) % slides.length;
+      update();
+    }
+
+    prevBtn.addEventListener("click", function () {
+      goTo(index - 1);
+    });
+
+    nextBtn.addEventListener("click", function () {
+      goTo(index + 1);
+    });
+
+    update();
+  });
+});
